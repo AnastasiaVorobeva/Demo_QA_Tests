f22d73e95239fc87cd9308f6fcc7f22dcf2c4c3a
anastasia.ev97@gmail.com <51174595+AnastasiaVorobeva@users.noreply.github.com>
2022-09-02 15:11:22 +0300

    refactor test_fill_practice_form_tools_qa

--- Lesson_5/test_fill_practice_form_tools_qa.py
+++ Lesson_5/test_fill_practice_form_tools_qa.py
@@ -1,16 +1,29 @@
-from selene import be
+from selene import be, have, command
 from selene.support.shared import browser
+from selene.support.shared.jquery_style import ss
+
+
+def given_opened_form():
+    browser.open('/automation-practice-form')
+    (
+        ss('[id^=google_ads][id$=container__]').with_(timeout=5)
+        .should(have.size_greater_than_or_equal(2))
+        .perform(command.js.remove)
+    )
 
 
 def test_fill_form_tools_qa(browser_management):
-    # Переход на страницу
-    browser.open('/automation-practice-form')
+    if browser.element('google_ads').is_displayed():
+        given_opened_form()
+    else:
+        browser.open('/automation-practice-form')
 
     # Личные данные
     browser.element('#firstName').should(be.blank).type('Anastasia')
     browser.element('#lastName').should(be.blank).type('Vorobeva')
-    browser.element('#userEmail').should(be.blank).type('anastasia.ev97@gmail.com')
+    browser.element('#userEmail').should(be.blank).type('anastasia.test@gmail.com')
     browser.element('#gender-radio-2+label').click()
+    browser.element('#userNumber').should(be.blank).type('9111111111')
 
     # Выбор даты рождения
     browser.element('[class="react-datepicker__input-container"]').click()
@@ -23,14 +36,20 @@ def test_fill_form_tools_qa(browser_management):
     # Дополнительная информация
     browser.element('#subjectsInput').type('maths').press_enter()
     browser.element('#hobbies-checkbox-1+label').click()
-    browser.element('#currentAddress').type('Moscow, Obrazcova st, 4')
+    browser.element('#currentAddress').type('Moscow')
     browser.element('#state').scroll_to().click()
     browser.element('#react-select-3-option-2').click()
     browser.element('#city').click()
     browser.element('#react-select-4-option-0').click()
 
-    # Ввод номера телфона и отпрака формы
-    browser.element('#userNumber').should(be.blank).type('9277548529').press_enter()
+    browser.element('#submit').perform(command.js.click)
 
-
-
+    browser.element('.table-responsive').all("table tr")[1].should(have.text('Anastasia Vorobeva'))
+    browser.element('.table-responsive').all("table tr")[2].should(have.text('anastasia.test@gmail.com'))
+    browser.element('.table-responsive').all("table tr")[3].should(have.text('Female'))
+    browser.element('.table-responsive').all("table tr")[4].should(have.text('9111111111'))
+    browser.element('.table-responsive').all("table tr")[5].should(have.text('14 August,1997'))
+    browser.element('.table-responsive').all("table tr")[6].should(have.text('Maths'))
+    browser.element('.table-responsive').all("table tr")[7].should(have.text('Sports'))
+    browser.element('.table-responsive').all("table tr")[9].should(have.text('Moscow'))
+    browser.element('.table-responsive').all("table tr")[10].should(have.text('Haryana Karnal'))
--- conftest.py
+++ conftest.py
@@ -2,16 +2,15 @@ import pytest
 from selene.support.shared import browser
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope='function', autouse=True)
 def window_size():
-    browser.open('https://google.com').driver.set_window_size(1920, 1080)
+    browser.open('https://google.com').driver.set_window_size(1920,1000)
     yield
     browser.quit()
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
-    browser.config.browser_name = 'chorme'
     browser.config.base_url = 'https://demoqa.com'
 
 
